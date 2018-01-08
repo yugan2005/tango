3748749f7e1cd93ff01b9bc5d146616b0d7d64ca
Yu Gan <yugan@DBGPYG-Yu.router>
2018-01-08 07:39:28 -0800

    finished charpter 6

--- rango/views.py
+++ rango/views.py
@@ -6,13 +6,16 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'pages': pages_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
     context_dict = {'myname': 'YG'}
     return render(request, 'rango/about.html', context=context_dict)
+
 
 def show_category(request, category_name_slug):
     context_dict = dict()
